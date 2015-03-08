81e96b6aaed1d4e7af698d02fceda801d01fd625
xunchen <powerson@163.com>
2015-03-08 14:25:29 +0800

    数据库日期格式修改

--- ror/app/models/customer.rb
+++ ror/app/models/customer.rb
@@ -1,7 +1,6 @@
 class Customer < ActiveRecord::Base
 	belongs_to :user
-	has_many :visit_plans，:order =>'visit_plan_date  desc'
+	has_many :visit_plans, :order =>'visit_plan_date  desc'
 	has_many :visit_records, :order =>'visit_date  desc'
 	validates :name, presence: true
-	attr_accessible :name
 end
